c71b5afb0e059c3ccb64179dac0888d8a8c27fe8
Bishalsarang <bishalsarang@gmail.com>
2020-06-29 22:18:27 +0545

    Minimal Working version with some bugs

--- JS Assignment-3 Box Collision/script/ball.js
+++ JS Assignment-3 Box Collision/script/ball.js
@@ -1,60 +1,59 @@
-const DIRX = [ -1, -1, -1, 0, 1, 1, 1, 0];
+const DIRX = [-1, -1, -1, 0, 1, 1, 1, 0];
 const DIRY = [-1, 0, 1, -1, 1, 0, -1, 1];
-class Ball{
-    constructor(x, y, radius, speed, color, direction) {
-        this.x = x;
-        this.y = y;
-        this.dx = 1;
-        this.dy = 1;
-        this.radius = radius;
-        this.speed = 1;
-        this.color = color;
-        this.direction = direction;
-    }
-    
+class Ball {
+	constructor(x, y, radius, speed, color, direction) {
+		this.x = x;
+		this.y = y;
+		this.dx = 1;
+		this.dy = 1;
+		this.radius = radius;
+		this.speed = speed;
+		this.color = color;
+		this.direction = direction;
+	}
 
-    move(){
-        this.x += DIRX[this.direction] * this.speed;
-        this.y +=  DIRY[this.direction] * this.speed;
-    }
+	move() {
+		this.x += DIRX[this.direction] * this.speed;
+		this.y += DIRY[this.direction] * this.speed;
+	}
 
-    exchangeDirection(ball){
-        console.log("Previous")
-        console.log(this);
-        console.log(ball);
-        let temp = this.direction;
-        this.direction = ball.direction;
-        ball.direction = temp;
-        console.log("After")
-        console.log(this);
-        console.log(ball);
-    }
+	exchangeDirection(ball) {
 
-    isWallCollision(width, height){
-        return this.x + this.radius >= width ||   this.x - this.radius <= 0 || this.y + this.radius >= height ||   this.y - this.radius <= 0
-    }
+		let temp = this.direction;
+		this.direction = ball.direction;
+		ball.direction = temp;
+	}
 
-    isCollide(ball){  
-        let dx = this.x - ball.x;
-        let dy = this.y - ball.y;
-        var distance = Math.sqrt(dx * dx + dy * dy);
-        if (distance < this.radius + ball.radius) {
-            return true;
-        }
-       
-        return false;
-    }
+	isWallCollision(width, height) {
+		return (
+			this.x + this.radius >= width ||
+			this.x - this.radius <= 0 ||
+			this.y + this.radius >= height ||
+			this.y - this.radius <= 0
+		);
+	}
 
-    doesCollide(ballList, index){
-        for(let i = 0; i < ballList.length; i++){
-            let {x, y, radius, speed, color}  = ballList[i];
-            if(i != index){
-                if(this.isCollide(ballList[i])){
-                    return true;
-                }
-            }
-        }
-        
-        return false;
-    }
+	isCollide(ball) {
+		let dx = this.x - ball.x;
+		let dy = this.y - ball.y;
+		var distance = Math.sqrt(dx * dx + dy * dy);
+		if (distance < this.radius + ball.radius) {
+			return true;
+		}
+
+		return false;
+	}
+
+	doesCollide(ballList, index) {
+		for (let i = 0; i < ballList.length; i++) {
+			let { x, y, radius, speed, color } = ballList[i];
+			if (i != index) {
+				if (this.isCollide(ballList[i])) {
+					return [true, ballList[i]];
+				}
+			}
+		}
+
+		return [false, null];
+	}
 }
--- JS Assignment-3 Box Collision/script/script.js
+++ JS Assignment-3 Box Collision/script/script.js
@@ -1,113 +1,122 @@
-
-
 var i = 0;
 class Canvas {
 	constructor(width, height, totalNumberOfBalls, canvasContainer) {
-        var that = this;
-        this.canvas = document.querySelector(canvasContainer);
-        this.canvasWidth = width;
-        this.canvasHeight = height;
+		var that = this;
+		this.canvas = document.querySelector(canvasContainer);
+		this.canvasWidth = width;
+		this.canvasHeight = height;
 
 		this.canvas.width = this.canvasWidth;
-        this.canvas.height = this.canvasHeight;
-        this.canvas.style.border = "1px solid red";
-        this.totalNumberOfBalls = totalNumberOfBalls;
-        this.ballList = [];
-        this.randomGenerator = new RandomPropertiesGenerator(this.canvasWidth, this.canvasHeight); 
+		this.canvas.height = this.canvasHeight;
+		this.canvas.style.border = '1px solid red';
+		this.totalNumberOfBalls = totalNumberOfBalls;
+		this.ballList = [];
+		this.randomGenerator = new RandomPropertiesGenerator(
+			this.canvasWidth,
+			this.canvasHeight
+		);
 
-        this.i = 0;
+		this.i = 0;
 	}
-    
+
 	init() {
-        let ballIndex = 0;
-        let radius = this.randomGenerator.getRandomInt(20, 8);
-            let [x, y] = this.randomGenerator.getRandomCoordinateBall(radius);
-            let ball = new Ball(100, 100, 5, 5, this.randomGenerator.getRandomColor(), 5);
-            this.ballList.push(ball)
+		let ballIndex = 0;
+		// let radius = this.randomGenerator.getRandomInt(20, 8);
+		//     let [x, y] = this.randomGenerator.getRandomCoordinateBall(radius);
+		//     let ball = new Ball(100, 100, 5, 5, this.randomGenerator.getRandomColor(), 5);
+		//     this.ballList.push(ball)
 
-             ball = new Ball(300, 100, 5, 5, this.randomGenerator.getRandomColor(), 1);
-            this.ballList.push(ball)
+		//      ball = new Ball(300, 100, 5, 5, this.randomGenerator.getRandomColor(), 1);
+		//     this.ballList.push(ball)
 
-            // ball = new Ball(this.canvas.width - 10, 10, radius, 5, this.randomGenerator.getRandomColor(), this.randomGenerator.getRandomInt(7, 0));
-            // this.ballList.push(ball)
-        // while(ballIndex < this.totalNumberOfBalls){
-        //     let radius = this.randomGenerator.getRandomInt(20, 8);
-        //     let [x, y] = this.randomGenerator.getRandomCoordinateBall(radius);
-        //     let ball = new Ball(x, y, radius, 5, this.randomGenerator.getRandomColor(), this.randomGenerator.getRandomInt(7, 0));
-        //     if(!ball.doesCollide(this.ballList, ballIndex)){
-        //         this.ballList.push(ball);
-        //         ballIndex++;
-        //     }	
-        // }		
-    }
-    
-    draw(){
-        let cx = this.canvas.getContext('2d');
-        
-        
-        cx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        
-        this.ballList.forEach((ball, index) =>{
-            let {x, y, radius, speed, color}  = ball;
-            ball.move();
+		// ball = new Ball(this.canvas.width - 10, 10, radius, 5, this.randomGenerator.getRandomColor(), this.randomGenerator.getRandomInt(7, 0));
+		// this.ballList.push(ball)
+		while (ballIndex < this.totalNumberOfBalls) {
+			let radius = this.randomGenerator.getRandomInt(20, 8);
+			let [x, y] = this.randomGenerator.getRandomCoordinateBall(radius);
+			let ball = new Ball(
+				x,
+				y,
+				radius,
+				5,
+				this.randomGenerator.getRandomColor(),
+				this.randomGenerator.getRandomInt(7, 0)
+			);
+			if (!ball.doesCollide(this.ballList, ballIndex)[0]) {
+				this.ballList.push(ball);
+				ballIndex++;
+			}
+		}
+	}
+
+	draw() {
+		let cx = this.canvas.getContext('2d');
+
+		cx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+		this.ballList.forEach((ball, index) => {
+			let { x, y, radius, speed, color } = ball;
+			ball.move();
+			// cx.beginPath();
+			// cx.arc(x, y, radius, 0, 360);
+			// cx.fillStyle = color;
+			// cx.fill();
+			// cx.closePath();
+			
+			let [isCollision, collidedBall] = ball.doesCollide(this.ballList, index);
+			if (isCollision) {
+                ball.exchangeDirection(collidedBall);
+                if (collidedBall.isWallCollision(this.canvas.width, this.canvas.height)) {
+                    collidedBall.direction = (collidedBall.direction + 4) % 8;
+                }
+                
+            }
+            if (ball.isWallCollision(this.canvas.width, this.canvas.height)) {
+				ball.direction = (ball.direction + 4) % 8;
+            }
+            
             cx.beginPath();
-                cx.arc(x, y , radius, 0, 360);
-                cx.fillStyle = color;
-                cx.fill();
-                cx.closePath();
-            if(ball.isWallCollision(this.canvas.width, this.canvas.height)){
-                ball.direction = (ball.direction + 4) % 8;
-            }
-            if(!ball.doesCollide(this.ballList, index)){
-                
-            }
-            else{
-                //Resolve COllision
-                console.log("hshshs");
-                // cancelAnimationFrame(this.animationId);
-                
-                ball.exchangeDirection(this.ballList[index]);
-            }
-           
-        });
-        window.requestAnimationFrame(this.draw.bind(this));
-    }
+			cx.arc(x, y, radius, 0, 360);
+			cx.fillStyle = color;
+			cx.fill();
+			cx.closePath();
+		});
+		window.requestAnimationFrame(this.draw.bind(this));
+	}
 
-    start(){
-        this.animationId = window.requestAnimationFrame(this.draw.bind(this));
-    }
-    
+	start() {
+		this.animationId = window.requestAnimationFrame(this.draw.bind(this));
+	}
 }
 
+class RandomPropertiesGenerator {
+	constructor(canvasWidth, canvasHeight) {
+		this.canvasWidth = canvasWidth;
+		this.canvasHeight = canvasHeight;
+	}
 
-class RandomPropertiesGenerator{
-    constructor(canvasWidth, canvasHeight){
-        this.canvasWidth = canvasWidth;
-        this.canvasHeight = canvasHeight;
-    }
+	getRandomInt(maxm, minm = 0) {
+		return Math.floor(Math.random() * (maxm - minm) + minm);
+	}
 
-    getRandomInt(maxm, minm=0){
-        return Math.floor(Math.random() * (maxm - minm) + minm);
+	getRandomColor() {
+		let r = this.getRandomInt(255),
+			g = this.getRandomInt(255),
+			b = this.getRandomInt(255);
+		return `rgb(${r},${g},${b})`;
+	}
 
-    }
+	getRandomCoordinateBall(radius) {
+		let maxm = this.canvasWidth - 2 * radius;
+		let minm = 2 * radius;
+		let x = this.getRandomInt(maxm, minm);
 
-    getRandomColor(){
-        let r = this.getRandomInt(255), g = this.getRandomInt(255), b = this.getRandomInt(255);
-        return `rgb(${r},${g},${b})`
-    }
-
-    getRandomCoordinateBall(radius){   
-        let maxm = this.canvasWidth - 2 * radius;
-        let minm = 2 * radius;
-        let x = this.getRandomInt(maxm, minm);
-
-        maxm =  this.canvasHeight - 2 * radius;
-        let y = this.getRandomInt(maxm, minm)
-        return [x , y];
-    }
+		maxm = this.canvasHeight - 2 * radius;
+		let y = this.getRandomInt(maxm, minm);
+		return [x, y];
+	}
 }
 
-cv = new Canvas(1024, 480, 2, 'canvas')
+cv = new Canvas(1024, 480, 20, 'canvas');
 cv.init();
 cv.start();
-
