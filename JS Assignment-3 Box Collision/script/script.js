e462957f0a3293717b45e1f0d074a675cb151cad
Bishalsarang <bishalsarang@gmail.com>
2020-06-29 13:48:20 +0545

    Basic drawing on canvas

--- JS Assignment-3 Box Collision/script/script.js
+++ JS Assignment-3 Box Collision/script/script.js
@@ -2,7 +2,27 @@ function getRandomCoordinates(topLeftX, topLeftY, canvasWidth, canvasHeight){
  
 }
 
-let totalNumberOfBalls = 10;
-for(let i = 0; i < totalNumberOfBalls; i++){
+let canvas = document.querySelector('canvas');
+canvas.width = 100;
+canvas.height = 100;
 
+let totalNumberOfBalls = 10;
+let ballList = []
+
+for(let i = 0; i < totalNumberOfBalls; i++){
+    ball = new Ball(50, 50, 10, "red");
+    ballList.push(ball);
 }
+
+console.log(ballList);
+
+function draw(){
+    
+    let cx = canvas.getContext('2d');
+    cx.fillStyle = 'red';
+    cx.beginPath();
+    cx.arc(50, 50, 40, 0, 360);
+    cx.fill();
+}
+
+draw()
