c97e78277bac3f8636bed6eaf7e0d1b947ed85a2
Bishalsarang <bishalsarang@gmail.com>
2020-06-29 18:07:06 +0545

    Add non overlapping circles

--- JS Assignment-3 Box Collision/script/ball.js
+++ JS Assignment-3 Box Collision/script/ball.js
@@ -6,4 +6,28 @@ class Ball{
         this.speed = speed;
         this.color = color;
     }
+    
+    isCollide(ball){  
+        let dx = this.x - ball.x;
+        let dy = this.y - ball.y;
+        var distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < this.radius + ball.radius) {
+            return true;
+        }
+       
+        return false;
+    }
+
+    doesCollide(ballList, index){
+        for(let i = 0; i < ballList.length; i++){
+            let {x, y, radius, speed, color}  = ballList[i];
+            if(i != index){
+                if(this.isCollide(ballList[i])){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
 }
--- JS Assignment-3 Box Collision/script/script.js
+++ JS Assignment-3 Box Collision/script/script.js
@@ -1,5 +1,10 @@
+const DIRX = [ 0 ,  1 , 1 , 1 , 0 , -1 , -1 , -1];
+const DIRY = [-1 , -1 , 0 , 1 , 1 ,  1 ,  0 , -1];
+
+
 class Canvas {
 	constructor(width, height, totalNumberOfBalls, canvasContainer) {
+        var that = this;
         this.canvas = document.querySelector(canvasContainer);
         this.canvasWidth = width;
         this.canvasHeight = height;
@@ -9,35 +14,68 @@ class Canvas {
         this.canvas.style.border = "1px solid red";
         this.totalNumberOfBalls = totalNumberOfBalls;
         this.ballList = [];
+        this.randomGenerator = new RandomPropertiesGenerator(this.canvasWidth, this.canvasHeight); 
 	}
-
-    getRandomCoordinates(){
-        return [Math.ceil(Math.random() * this.canvasWidth), Math.ceil(Math.random() * this.canvasHeight)];
-    }
     
 	init() {
-		for (let i = 0; i < this.totalNumberOfBalls; i++) {
-            let [x, y] = this.getRandomCoordinates();
-			let ball = new Ball(x, y, 5, 5, 'red');
-			this.ballList.push(ball);
-		}
+        let ballIndex = 0;
+        while(ballIndex < this.totalNumberOfBalls){
+            let radius = this.randomGenerator.getRandomInt(20, 8);
+            let [x, y] = this.randomGenerator.getRandomCoordinateBall(radius);
+            let ball = new Ball(x, y, radius, 5, this.randomGenerator.getRandomColor());
+            if(!ball.doesCollide(this.ballList, ballIndex)){
+                this.ballList.push(ball);
+                ballIndex++;
+            }	
+        }		
     }
     
     draw(){
         let cx = this.canvas.getContext('2d');
-        this.ballList.forEach(function(ball){
-            let {x, y, radius, speed, color} = ball;
-            cx.fillStyle = color;
-            cx.beginPath();
-            cx.arc(x, y, radius, 0, 360);
-            cx.fill();
-        })
-        
+        for(let i = 0; i < this.ballList.length; i++){
+            let {x, y, radius, speed, color}  = this.ballList[i];
+            if(!this.ballList[i].doesCollide(this.ballList, i)){
+                cx.fillStyle = color;
+                cx.beginPath();
+                cx.arc(x, y, radius, 0, 360);
+                cx.fill();
+            }
+        }
     }
 }
 
 
-cv = new Canvas(300, 300, 10, 'canvas')
+class RandomPropertiesGenerator{
+    constructor(canvasWidth, canvasHeight){
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+    }
+
+    getRandomInt(maxm, minm=0){
+        return Math.floor(Math.random() * (maxm - minm) + minm);
+
+    }
+
+    getRandomColor(){
+        let r = this.getRandomInt(255), g = this.getRandomInt(255), b = this.getRandomInt(255);
+        return `rgb(${r},${g},${b})`
+    }
+
+    getRandomCoordinateBall(radius){   
+        let maxm = this.canvasWidth - 2 * radius;
+        let minm = 2 * radius;
+        let x = this.getRandomInt(maxm, minm);
+
+        maxm =  this.canvasHeight - 2 * radius;
+        let y = this.getRandomInt(maxm, minm)
+        return [x , y];
+    }
+}
+
+cv = new Canvas(1024, 480, 600, 'canvas')
 cv.init();
 cv.draw();
 
+set = new Set();
+set.add([1, 2]);
+set.add([1, 2]);
