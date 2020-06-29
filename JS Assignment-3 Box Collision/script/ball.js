19469a53e4477ffc2193fbc5ec0e1b1b438ff6d2
Bishalsarang <bishalsarang@gmail.com>
2020-06-29 14:35:40 +0545

    Add methods draw, init, getRandomCoordinates

--- JS Assignment-3 Box Collision/script/ball.js
+++ JS Assignment-3 Box Collision/script/ball.js
@@ -1,7 +1,8 @@
 class Ball{
-    constructor(x, y, speed, color) {
+    constructor(x, y, radius, speed, color) {
         this.x = x;
         this.y = y;
+        this.radius = radius;
         this.speed = speed;
         this.color = color;
     }
--- JS Assignment-3 Box Collision/script/script.js
+++ JS Assignment-3 Box Collision/script/script.js
@@ -1,28 +1,43 @@
-function getRandomCoordinates(topLeftX, topLeftY, canvasWidth, canvasHeight){
- 
-}
+class Canvas {
+	constructor(width, height, totalNumberOfBalls, canvasContainer) {
+        this.canvas = document.querySelector(canvasContainer);
+        this.canvasWidth = width;
+        this.canvasHeight = height;
 
-let canvas = document.querySelector('canvas');
-canvas.width = 100;
-canvas.height = 100;
+		this.canvas.width = this.canvasWidth;
+        this.canvas.height = this.canvasHeight;
+        this.canvas.style.border = "1px solid red";
+        this.totalNumberOfBalls = totalNumberOfBalls;
+        this.ballList = [];
+	}
 
-let totalNumberOfBalls = 10;
-let ballList = []
-
-for(let i = 0; i < totalNumberOfBalls; i++){
-    ball = new Ball(50, 50, 10, "red");
-    ballList.push(ball);
-}
-
-console.log(ballList);
-
-function draw(){
+    getRandomCoordinates(){
+        return [Math.ceil(Math.random() * this.canvasWidth), Math.ceil(Math.random() * this.canvasHeight)];
+    }
     
-    let cx = canvas.getContext('2d');
-    cx.fillStyle = 'red';
-    cx.beginPath();
-    cx.arc(50, 50, 40, 0, 360);
-    cx.fill();
+	init() {
+		for (let i = 0; i < this.totalNumberOfBalls; i++) {
+            let [x, y] = this.getRandomCoordinates();
+			let ball = new Ball(x, y, 5, 5, 'red');
+			this.ballList.push(ball);
+		}
+    }
+    
+    draw(){
+        let cx = this.canvas.getContext('2d');
+        this.ballList.forEach(function(ball){
+            let {x, y, radius, speed, color} = ball;
+            cx.fillStyle = color;
+            cx.beginPath();
+            cx.arc(x, y, radius, 0, 360);
+            cx.fill();
+        })
+        
+    }
 }
 
-draw()
+
+cv = new Canvas(300, 300, 10, 'canvas')
+cv.init();
+cv.draw();
+
