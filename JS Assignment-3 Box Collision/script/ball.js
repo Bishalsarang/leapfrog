c56fdea6cd77a2b961dc393ee9b013b1b838415c
Bishalsarang <bishalsarang@gmail.com>
2020-06-29 13:37:17 +0545

    Add Ball class

---
+++ JS Assignment-3 Box Collision/script/ball.js
@@ -0,0 +1,8 @@
+class Ball{
+    constructor(x, y, speed, color) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.color = color;
+    }
+}
---
+++ JS Assignment-3 Box Collision/script/script.js
@@ -0,0 +1,8 @@
+function getRandomCoordinates(topLeftX, topLeftY, canvasWidth, canvasHeight){
+ 
+}
+
+let totalNumberOfBalls = 10;
+for(let i = 0; i < totalNumberOfBalls; i++){
+
+}
