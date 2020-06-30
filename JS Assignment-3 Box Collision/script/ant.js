bec8180724d3fa1aab61b9138502444ecd921c3a
Bishalsarang <bishalsarang@gmail.com>
2020-06-30 06:32:00 +0545

    Add collision detection feature from ball colllision

--- JS Assignment-3 Box Collision/script/ant.js
+++ JS Assignment-3 Box Collision/script/ant.js
@@ -1,21 +1,37 @@
 let canvas = document.querySelector('.world');
-canvas.style.width = 500 + 'px';
-canvas.style.height = 500 + 'px';
+const WIDTH = 600;
+const HEIGHT = 600;
+
+canvas.style.width = WIDTH + 'px';
+canvas.style.height = WIDTH + 'px';
 canvas.style.position = 'relative';
 
 ANT_IMAGE_PATH = 'images/ant.png';
+const ANT_WIDTH = 30;
+const ANT_HEIGHT = 40;
+
 class Ant{
     constructor(x, y){
         this.x = x;
         this.y = y;
+        this.speed = 1;
         this.el = document.createElement('img');
         this.el.src = ANT_IMAGE_PATH;
-        this.el.style.width = 10 + 'px';
-        this.el.style.height = 20 + 'px';
+        
+        this.el.style.width = ANT_WIDTH + 'px';
+        this.el.style.height = ANT_HEIGHT + 'px';
 
         this.el.style.left = x + 'px';
         this.el.style.top = y + 'px';
         this.el.style.position = 'relative';
+
+        this.el.addEventListener('click',() => {
+            this.el.src= "images/splash.png";
+        });
+      
+    }
+
+    create(){
         canvas.appendChild(this.el)
     }
 
@@ -24,7 +40,75 @@ class Ant{
         window.requestAnimationFrame(this.move.bind(this));
     }
     
+
+    isWallCollision(width, height) {
+        if(this.x + this.radius >= width ){
+            this.x = width - this.radius;
+            this.dx = -this.dx;
+        }
+        if(this.x - this.radius <= 0){
+            this.x = this.radius;
+            this.dx = -this.dx;
+        }
+        if(this.y - this.radius <= 0){
+            this.y = this.radius;
+            this.dy = -this.dy;
+        }
+        if(this.y + this.radius > height){
+            this.y = height - this.radius;
+            this.dy = -this.dy;
+        }
+		return (
+			this.x + this.radius >= width ||
+			this.x - this.radius <= 0 ||
+			this.y + this.radius >= height ||
+			this.y - this.radius <= 0
+		);
+	}
+
+    collidesWIth(antList){
+
+        antList.forEach((ant, index) => {
+            if(index != this){
+                if(this.x < ant.x + ant.width &&
+                    this.x + this.width > ant.x &&
+                    this.y < ant.y + ant.height &&
+                    this.y + this.height > ant.y){
+                        return true;
+                    }
+            }
+
+        });
+        return false;
+    }
 }
 
-ant1 = new Ant(10, 10);
-ant1.move();
+
+function getRandomInt(maxm, minm = 0) {
+    return Math.floor(Math.random() * (maxm - minm) + minm);
+}
+
+let numberOfAnts = 20;
+antList = [];
+function generateAnts(){
+    for(let i = 0; i < numberOfAnts; i++){
+        let x = getRandomInt(720 - ANT_WIDTH, 0 + ANT_WIDTH);
+        let y = getRandomInt(480 - ANT_HEIGHT, 0 + ANT_HEIGHT);
+        ant = new Ant(x, y);
+        if(!ant.collidesWIth(antList)){
+            antList.push(ant);   
+         }   
+         else{
+             i--;
+         }
+         ant.create();
+         }
+        
+    }
+
+
+
+
+
+generateAnts();
+// ant.move();
