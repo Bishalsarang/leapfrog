1f78f8ca40a8d86749ef5f67cff60b20889fa412
Bishalsarang <bishalsarang@gmail.com>
2020-06-30 09:10:33 +0545

    Add ant collision

--- JS Assignment-3 Box Collision/script/ant.js
+++ JS Assignment-3 Box Collision/script/ant.js
@@ -1,6 +1,6 @@
 let canvas = document.querySelector('.world');
-const WIDTH = 1000;
-const HEIGHT = 600;
+const WIDTH = 400;
+const HEIGHT = 400;
 
 canvas.style.width = WIDTH + 'px';
 canvas.style.height = HEIGHT + 'px';
@@ -8,9 +8,12 @@ canvas.style.position = 'relative';
 canvas.style.border = '1px solid red';
 
 ANT_IMAGE_PATH = 'images/ant.png';
-const ANT_WIDTH = 30;
-const ANT_HEIGHT = 40;
+const RADIUS = 7;
+const ANT_WIDTH = RADIUS * 4;
+const ANT_HEIGHT = RADIUS * 4;
 
+
+var antList = [];
 class Ant{
     constructor(x, y){
         this.x = x;
@@ -20,27 +23,42 @@ class Ant{
         this.speed = 1;
         this.el = document.createElement('img');
         this.el.src = ANT_IMAGE_PATH;
-        
+        this.radius = 10;
         this.el.style.width = ANT_WIDTH + 'px';
         this.el.style.height = ANT_HEIGHT + 'px';
 
         this.el.style.left = x + 'px';
         this.el.style.top = y + 'px';
+        this.el.style.zIndex = '10';
         this.el.style.position = 'absolute';
 
         this.el.addEventListener('click',(e) => {
             
             this.el.src= "images/splash.png";
-            canvas.removeChild(this.el);
+            this.remove();
         });
       
     }
 
     remove(){
+        
+        canvas.removeChild(this.el);
+        antList = antList.filter((ant) =>{
+            return ant != this;
+        })
 
     }
     create(){
         canvas.appendChild(this.el)
+    }
+
+    stillCollide(ant){
+        return (this.x < ant.x + ANT_WIDTH &&
+            this.x + ANT_WIDTH > ant.x &&
+            this.y < ant.y + ANT_HEIGHT &&
+            this.y + ANT_HEIGHT > ant.y);
+            
+
     }
 
     draw(){
@@ -48,16 +66,19 @@ class Ant{
         let [collides, ant] = this.collidesWIth(antList);
 
         if(collides){
-            console.log("here", this);
-            console.log("hire", ant);
             this.dx = -this.dx;
             this.dy = -this.dy;
 
             ant.dx = -ant.dx;
             ant.dy = -ant.dy;
+            
+            this.x += this.dx * this.speed;
+        this.y += this.dy * this.speed;
+            
         }
         this.x += this.dx * this.speed;
         this.y += this.dy * this.speed;
+        
     }
 
     move(){ 
@@ -69,14 +90,14 @@ class Ant{
 
     isWallCollision(width, height) {
         if(this.x + ANT_WIDTH >= WIDTH ){
-            this.x = WIDTH - 1.5 * ANT_WIDTH;
+            this.x = WIDTH - ANT_WIDTH;
             this.dx = -this.dx;
         }
-        if(this.x - ANT_WIDTH <= 0){
+        if(this.x <= 0){
             this.x = ANT_WIDTH;
             this.dx = -this.dx;
         }
-        if(this.y - ANT_HEIGHT<= 0){
+        if(this.y<= 0){
             this.y = ANT_HEIGHT;
             this.dy = -this.dy;
         }
@@ -84,23 +105,18 @@ class Ant{
             this.y = HEIGHT - ANT_HEIGHT;
             this.dy = -this.dy;
         }
-		// return (
-		// 	this.x + this.radius >= width ||
-		// 	this.x - this.radius <= 0 ||
-		// 	this.y + this.radius >= height ||
-		// 	this.y - this.radius <= 0
-		// );
 	}
 
     collidesWIth(antList){
         for(let i = 0; i < antList.length; i++){
+            
             let ant = antList[i];
             if(ant != this){
+               
                 if(this.x < ant.x + ANT_WIDTH &&
                     this.x + ANT_WIDTH > ant.x &&
                     this.y < ant.y + ANT_HEIGHT &&
                     this.y + ANT_HEIGHT > ant.y){
-                        console.log("collision");
                         return [true, ant];
                         
                     }
@@ -116,8 +132,8 @@ function getRandomInt(maxm, minm = 0) {
     return Math.floor(Math.random() * (maxm - minm) + minm);
 }
 
-let numberOfAnts = 10;
-antList = [];
+let numberOfAnts = 8;
+
 function generateAnts(){
     for(let i = 0; i < numberOfAnts; i++){
         let x = getRandomInt(WIDTH - ANT_WIDTH, 0 + ANT_WIDTH);
@@ -135,17 +151,10 @@ function generateAnts(){
     }
 
 function render(){
-    console.log("sss");
     antList.forEach((ant, index) => {
        ant.draw();
        ant.move();
     });
     window.requestAnimationFrame(render);
 }
-
-
-
-
-
 generateAnts();
-// ant.move();
