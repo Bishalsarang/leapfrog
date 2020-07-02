d1693910bec17522237f909aa0f153229d84920a
Bishalsarang <bishalsarang@gmail.com>
2020-07-02 22:50:09 +0545

    Update parameter for general animation

--- Helix Animation/script/animation.js
+++ Helix Animation/script/animation.js
@@ -6,23 +6,23 @@ let ctx = canvas.getContext('2d');
 let tick = 0;
 let beginX = 50;
 let beginY = 50;
+let speed = 0.2;
 
 function draw() {
-	let colOffset = 0;
-	let phase = tick * 0.2;
+	let rowOffset = 0;
+	let phase = tick * speed;
 	for (let strand = 0; strand < 2; strand++) {
 		strandPhase = phase + strand * Math.PI;
-		x = beginX;
+
 		for (let col = 0; col < NUM_COLUMNS; col++) {
-			x += 2 * MAX_RADIUS;
-			colOffset = col / MAX_RADIUS;
-			for (let row = 0; row < NUM_COLUMNS; row++) {
-				let y =
-					beginY +
-					row * 2 * MAX_RADIUS +
-          (Math.sin(strandPhase + colOffset) + 1) * 30;
-        let rowOffset = row / MAX_RADIUS;
-				let circleRadius = (Math.cos(strandPhase + colOffset) + 1)  * rowOffset;
+			x = beginX + col * OFFSET;
+			rowOffset = (col * 2 * Math.PI) / (NUM_COLUMNS * 2);
+			for (let row = 0; row < NUM_ROWS; row++) {
+				let y = beginY + row * OFFSET + Math.sin(strandPhase + rowOffset) * 25;
+				let circleRadius =
+					(Math.cos(strandPhase - row * 0.1 + rowOffset) + 1) *
+					0.5 *
+					MAX_RADIUS;
 				let circle = new Circle(x, y, circleRadius, ctx);
 				circle.draw();
 			}
--- Helix Animation/script/constants.js
+++ Helix Animation/script/constants.js
@@ -1,6 +1,6 @@
 const CANVAS_WIDTH = 300;
 const CANVAS_HEIGHT = 300;
-const NUM_ROWS = 15;
+const NUM_ROWS = 10;
 const NUM_COLUMNS = 15;
 const MAX_RADIUS = 5;
-const OFFSET = 10;
+const OFFSET = MAX_RADIUS * 2;
